c7a8b371b33a4bb49834da150946621b93d31b15
dikshakumari78 <sharmadiksha7870@gmail.com>
2024-08-31 21:01:36 +0530

    Create threadpool.h

---
+++ codes/threadpool.h
@@ -0,0 +1,46 @@
+#ifndef THREADPOOL_H
+#define THREADPOOL_H
+
+#include <pthread.h>
+#include <queue>
+#include <functional>
+#include <vector>
+
+// Task class
+class Task {
+public:
+    std::function<void(void*)> function;
+    void *arg;
+};
+
+// TaskQueue class
+class TaskQueue {
+private:
+    std::queue<Task> queue;
+    pthread_mutex_t lock;
+    pthread_cond_t notify;
+    bool shutdown;
+
+public:
+    TaskQueue();
+    ~TaskQueue();
+    void add_task(const Task& task);
+    bool get_task(Task& task);
+    void set_shutdown();
+};
+
+// ThreadPool class
+class ThreadPool {
+private:
+    std::vector<pthread_t> threads;
+    TaskQueue taskQueue;
+
+    static void* thread_pool_worker(void* arg);
+
+public:
+    ThreadPool(int threadCount);
+    ~ThreadPool();
+    void add_task(const std::function<void(void*)>& func, void* arg);
+};
+
+#endif // THREADPOOL_H
